0d784ac6d7aafb918d565b2e28cde33988c9583a
Alpenjugo <milenko.kovacevic@hotmail.com>
2025-04-02 14:09:00 +0200

    ✅ Refactor User.js for injected MongoDB instance

--- src/models/User.js
+++ src/models/User.js
@@ -1,94 +1,65 @@
 const validators = require('./../utils/validators');
-const database = require('./../db/mongoose');
 const constants = require('./../utils/constants');
 const encryptDecrypter = require('./../utils/encrypterDecrypter');
 const tokenGenerator = require('./../utils/tokenGenerator');
 
 class User {
-   /**
-    * _userId
-    * _firstName
-    * _nickName
-    * _email
-    * @param userId
-    * @param firstName
-    * @param nickName
-    * @param email
-    */
-   constructor(userId, firstName, nickName, email) {
-      this._userId = validators.validateString(userId) ? userId : false;
-      this._firstName = validators.validateString(firstName) ? firstName : false;
-      this._nickName = validators.validateString(nickName) ? nickName : false;
-      this._email = validators.validateEmail(email) ? email : false;
-   }
+  /**
+   * Konstruktor
+   * @param db MongoDB-Instanz (über app.locals.db übergeben)
+   * @param userId
+   * @param firstName
+   * @param nickName
+   * @param email
+   */
+  constructor(db, userId, firstName, nickName, email) {
+    this.db = db;
+    this._userId = validators.validateString(userId) ? userId : false;
+    this._firstName = validators.validateString(firstName) ? firstName : false;
+    this._nickName = validators.validateString(nickName) ? nickName : false;
+    this._email = validators.validateEmail(email) ? email : false;
+  }
 
-   /**
-    * Method to create the user.
-    * @param password: The password of the user.
-    * @returns {Promise<unknown>}
-    */
-   createUser(password) {
-      return new Promise((resolve, reject) => {
-         const userObj = {};
-         userObj[constants.USERS_FIRST_NAME] = this._firstName;
-         userObj[constants.USERS_EMAIL] = this._email;
-         userObj[constants.USERS_NICK_NAME] = this._nickName;
-         userObj[constants.USERS_PASSWORD] = encryptDecrypter.encrypt(password);
+  /**
+   * Benutzer anlegen
+   * @param password
+   * @returns {Promise<string>}
+   */
+  async createUser(password) {
+    const userObj = {};
+    userObj[constants.USERS_FIRST_NAME] = this._firstName;
+    userObj[constants.USERS_EMAIL] = this._email;
+    userObj[constants.USERS_NICK_NAME] = this._nickName;
+    userObj[constants.USERS_PASSWORD] = encryptDecrypter.encrypt(password);
 
-         database().then(db => {
-            db.collection(constants.USERS_COLLECTION).insertOne(userObj).then(_resultSet => {
-               console.log(_resultSet.insertedId);
-               resolve(_resultSet.insertedId);
-            }).catch(err => {
-               console.error(err);
-               reject(err);
-            });
-         });
-      });
-   }
+    const result = await this.db.collection(constants.USERS_COLLECTION).insertOne(userObj);
+    console.log('✅ User inserted with ID:', result.insertedId);
+    return result.insertedId;
+  }
 
-   getUsers() {
-      return new Promise((resolve, reject) => {
-         //TODO: Search for users. 
-      });
-   }
+  /**
+   * Benutzer-Login prüfen
+   * @param password
+   * @returns {Promise<Object>}
+   */
+  async checkAuthentication(password) {
+    const filter = {};
+    filter[constants.USERS_EMAIL] = this._email;
+    filter[constants.USERS_PASSWORD] = encryptDecrypter.encrypt(password);
 
-   /**
-    * Method to Check the Login credentials of a user and validate.
-    * @param password: The Password of the user.
-    * @returns {Promise<unknown>}
-    */
-   checkAuthentication(password) {
-      return new Promise((resolve, reject) => {
-         const filterClaus = {};
-         filterClaus[constants.USERS_EMAIL] = this._email;
-         filterClaus[constants.USERS_PASSWORD] = encryptDecrypter.encrypt(password);
-         
-         database().then(db => {
-            db.collection(constants.USERS_COLLECTION).find(filterClaus).toArray((err, _result) => {
-               if (err) {
-                  console.error(er);
-                  reject(err);
-               } else {
-                  const result = _result[0];
-                  if (validators.validateUndefined(result)) {
-                     let userDetails = {};
-                     delete result[constants.USERS_PASSWORD];
-                     userDetails[constants.JWT_TOKEN] = tokenGenerator.generateToken((result));
-                     userDetails[constants.USER_DETAILS] = result;
-                     resolve(userDetails);
-                  } else {
-                     resolve({});
-                  }
-               }
-            });
-         });
-      });
-   }
+    const result = await this.db.collection(constants.USERS_COLLECTION).findOne(filter);
+
+    if (validators.validateUndefined(result)) {
+      delete result[constants.USERS_PASSWORD];
+
+      return {
+        [constants.JWT_TOKEN]: tokenGenerator.generateToken(result),
+        [constants.USER_DETAILS]: result
+      };
+    } else {
+      return {};
+    }
+  }
 }
 
-/**
- * Exporting the user.
- * @type {User}
- */
 module.exports = User;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,55 +3,61 @@ const validator = require('./../utils/validators');
 const constants = require('./../utils/constants');
 const responseGenerator = require('./../utils/responseGenerator');
 const User = require('../models/User');
-const auth = require('../middlewares/auth')
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 /**
  * Get the user name to display on the navbar
  */
-router.get('/user', auth, (req,res)=>{
-   res.send(req.user)
-})
+router.get('/user', auth, (req, res) => {
+  res.send(req.user);
+});
 
 /**
  * Method to handle the registration of the user.
  */
 router.post('/users', async (req, res) => {
-   const firstName = validator.validateString(req.body[constants.USERS_FIRST_NAME]) ? req.body[constants.USERS_FIRST_NAME] : false;
-   const nickName = validator.validateString(req.body[constants.USERS_NICK_NAME]) ? req.body[constants.USERS_NICK_NAME] : false;
-   const email = validator.validateEmail(req.body[constants.USERS_EMAIL]) ? req.body[constants.USERS_EMAIL] : false;
-   const password = validator.validateString(req.body[constants.USERS_PASSWORD]) ? req.body[constants.USERS_PASSWORD] : false;
-   if (firstName && nickName && email && password) {
-      const user = new User(false, firstName, nickName, email);
-      user.createUser(password).then((userID) => {
-         res.status(constants.HTTP_SUCCESS).json(responseGenerator.generateResponse(userID));
-      }).catch(err => { res.status(constants.INTERNAL_SERVER_ERROR_CODE).json(responseGenerator.generateResponse(constants.ERROR_MESSAGE));
+  const firstName = validator.validateString(req.body[constants.USERS_FIRST_NAME]) ? req.body[constants.USERS_FIRST_NAME] : false;
+  const nickName = validator.validateString(req.body[constants.USERS_NICK_NAME]) ? req.body[constants.USERS_NICK_NAME] : false;
+  const email = validator.validateEmail(req.body[constants.USERS_EMAIL]) ? req.body[constants.USERS_EMAIL] : false;
+  const password = validator.validateString(req.body[constants.USERS_PASSWORD]) ? req.body[constants.USERS_PASSWORD] : false;
 
-      });
-   } else {
-      res.status(constants.BAD_REQUEST_CODE).json(responseGenerator.generateResponse(constants.INSUFFICIENT_DATA_MESSAGE));
-   }
+  if (firstName && nickName && email && password) {
+    try {
+      const db = req.app.locals.db; // 💥 Zugriff auf MongoDB-Verbindung
+      const user = new User(db, false, firstName, nickName, email);
+      const userID = await user.createUser(password);
+      res.status(constants.HTTP_SUCCESS).json(responseGenerator.generateResponse(userID));
+    } catch (err) {
+      console.error(err);
+      res.status(constants.INTERNAL_SERVER_ERROR_CODE).json(responseGenerator.generateResponse(constants.ERROR_MESSAGE));
+    }
+  } else {
+    res.status(constants.BAD_REQUEST_CODE).json(responseGenerator.generateResponse(constants.INSUFFICIENT_DATA_MESSAGE));
+  }
 });
 
 /**
  * Method to handle the login request of a user.
  */
 router.post('/users/login', async (req, res) => {
-   const email = validator.validateEmail(req.body[constants.USERS_EMAIL]) ? req.body[constants.USERS_EMAIL] : false;
-   const password = validator.validateString(req.body[constants.USERS_PASSWORD]) ? req.body[constants.USERS_PASSWORD] : false;
-   
-   if (email && password) {
-      const user = new User(false, false, false, email);
-      user.checkAuthentication(password).then(userData => {
-         res.status(constants.HTTP_SUCCESS).json(responseGenerator.generateResponse(userData));
-      }).catch(err => {
-         console.error(err);
-         res.status(constants.INTERNAL_SERVER_ERROR_CODE).json(responseGenerator.generateResponse(constants.ERROR_MESSAGE));
-      });
-   } else {
-      res.status(constants.BAD_REQUEST_CODE).json(responseGenerator.generateResponse(constants.INSUFFICIENT_DATA_MESSAGE));
-   }
+  const email = validator.validateEmail(req.body[constants.USERS_EMAIL]) ? req.body[constants.USERS_EMAIL] : false;
+  const password = validator.validateString(req.body[constants.USERS_PASSWORD]) ? req.body[constants.USERS_PASSWORD] : false;
+
+  if (email && password) {
+    try {
+      const db = req.app.locals.db; // 💥 auch hier MongoDB verfügbar
+      const user = new User(db, false, false, false, email);
+      const userData = await user.checkAuthentication(password);
+      res.status(constants.HTTP_SUCCESS).json(responseGenerator.generateResponse(userData));
+    } catch (err) {
+      console.error(err);
+      res.status(constants.INTERNAL_SERVER_ERROR_CODE).json(responseGenerator.generateResponse(constants.ERROR_MESSAGE));
+    }
+  } else {
+    res.status(constants.BAD_REQUEST_CODE).json(responseGenerator.generateResponse(constants.INSUFFICIENT_DATA_MESSAGE));
+  }
 });
 
 module.exports = router;
